01f5f849a39263dcb697f538f20b85b4eeb129f8
Bog0sso <sambe2000@hotmail.com>
2022-08-13 15:29:45 +0000

    setup| home page set - css to do

---
+++ frontend-medworld/src/lib/outfit/outfit.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let imageURL:string;
+    export let outfitLabel:string;
+</script>
+
+<div class="border-2 md:w-8/12 w- h-1/10 flex-col items-center">
+    <h3 class="underline">{outfitLabel}</h3>
+    <img src="{imageURL}" alt="{outfitLabel}">
+</div>
--- frontend-medworld/src/routes/index.svelte
+++ frontend-medworld/src/routes/index.svelte
@@ -1,1 +1,11 @@
-<h1 class="text-3xl font-bold bg-zinc-600 ">Hello world!</h1>
+<!-- <h1 class="text-3xl font-bold bg-white ">Hello world!</h1> -->
+
+<script lang="ts">
+    import Outfit from "../lib/outfit/outfit.svelte";
+</script>
+
+<main>
+    <h1 class="font-['Open_Sans'] font-bold	uppercase text-3xl text-center">Quelle tenue souhaiteriez-vous commander ?</h1>
+    <Outfit outfitLabel="Blouse" imageURL="/ressources/blouse.png"/>
+    <Outfit outfitLabel="Tenue de bloc" imageURL="/ressources/tenue-de-bloc.png"/>
+</main>
