114affb5cc87dbfcbe6db2c8caa0b82d478113af
Bog0sso <sambe2000@hotmail.com>
2022-09-15 02:07:06 +0000

    ui | top navbar added

--- frontend-medworld/src/routes/index.svelte
+++ frontend-medworld/src/routes/index.svelte
@@ -2,8 +2,10 @@
     import OutfitWrapper from "$lib/outfit-wrapper/outfit-wrapper.svelte";
     import TenueBlocForm from "$lib/forms/tenue-bloc-form/tenue-bloc-form.svelte";
     import BlouseForm from "$lib/forms/blouse-form/blouse-form.svelte";
+import Navbar from "$lib/navbar/navbar.svelte";
 </script>
 
 <!-- <OutfitWrapper/> -->
 <!-- <TenueBlocForm /> -->
-<BlouseForm/>
+<!-- <BlouseForm/> -->
+<Navbar/>
